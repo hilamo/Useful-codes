0b3a49690d4dbe565ad1f27bf69a95b4b5082dc4
hilamo <hilafd@gmail.com>
2016-06-09 11:36:10 +0300

    documentation added

--- Forms/wpcf7 functions with rav-messer.php
+++ Forms/wpcf7 functions with rav-messer.php
@@ -1,7 +1,7 @@
-<?php
+<?php 
 function mycf7_before_send_mail($WPCF7_ContactForm) {
     $form_id = $WPCF7_ContactForm->id;
-    $forms_api = get_field("forms","option");
+    $forms_api = get_field("forms","option"); // ACF repeater field which holds and Form Object and rav_messer form ID
     $submission = WPCF7_Submission::get_instance();
 
     foreach($forms_api as $form){
@@ -15,8 +15,8 @@ function mycf7_before_send_mail($WPCF7_ContactForm) {
 
     $mail = $WPCF7_ContactForm->prop('mail');
 
-    $mail['body'] = str_replace("[post_url]",'כתובת עמוד: '.urldecode($submission->get_meta("url")),$mail['body']);
-    $mail['body'] = str_replace("[post_title]",'כותרת עמוד: '.urldecode($post->post_title),$mail['body']);
+    $mail['body'] = str_replace("[post_url]",'כתובת עמוד: '.urldecode($submission->get_meta("url")),$mail['body']); // Only translation
+    $mail['body'] = str_replace("[post_title]",'כותרת עמוד: '.urldecode($post->post_title),$mail['body']); // Only translation
 
 	$WPCF7_ContactForm->set_properties( array( 'mail' => $mail ) );
 
@@ -25,14 +25,16 @@ function mycf7_before_send_mail($WPCF7_ContactForm) {
         $url = $submission->get_meta( 'url' );
         $params = $submission->get_posted_data();
 
-        $params["כתובת הפוסט"] = $url;
-        $params["שם הפוסט"] = $post->post_title;
+        $params["כתובת הפוסט"] = $url; // add custom parameter to submission
+        $params["שם הפוסט"] = $post->post_title; // add custom parameter to submission
 
-        send_to_rav_messer($params,$rav_messer_form_id);
+        send_to_rav_messer($params,$rav_messer_form_id); // send mail to rav messer with new parameters
 
     }
 }
 add_action( "wpcf7_before_send_mail", "mycf7_before_send_mail" );
+
+/********************************************************************************/
 
 function send_to_rav_messer($params,$form_id){
     $url = "https://cp.responder.co.il/subscribe.php";
@@ -48,7 +50,7 @@ function send_to_rav_messer($params,$form_id){
         $new_params["fields[".$key."]"] = $param;
     }
 
-    $new_params["form_id"] = $form_id;
+    $new_params["form_id"] = $form_id; 
 
     $params_string = http_build_query($new_params);
 
@@ -65,12 +67,15 @@ function send_to_rav_messer($params,$form_id){
     //close connection
     curl_close($ch);
 }
+
+/********************************************************************************/
+
 function get_post_id_from_wpcf7($submission){
     $unit_tag = $submission->get_meta( 'unit_tag' );
 
   if ( $unit_tag  && preg_match( '/^wpcf7-f(\d+)-p(\d+)-o(\d+)$/', $unit_tag, $matches ) ) {
-      $post_id = absint( $matches[2] );
-
+		$post_id = absint( $matches[2] );  //  $unit_tag for example: _wpcf7_unit_tag:wpcf7-f4-p6-o1. "f" is the form ID, "p" is the post ID that holds the form
+	
       if ( $post = get_post( $post_id ) ) {
               $the_post_id = (string) $post->ID;
               $the_post_name = $post->post_name;
