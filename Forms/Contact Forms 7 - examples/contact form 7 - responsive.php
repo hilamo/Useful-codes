1f618eb73954148e5c547b3d6f6500e297b6a1b1
Hila Moalem <hila.moalem72@gmail.com>
2018-12-03 09:58:21 +0200

    Add forms and email-html

---
+++ Forms/Contact Forms 7 - examples/contact form 7 - responsive.php
@@ -0,0 +1,28 @@
+<!-- Form with "Bootstrap" framework -->
+<div class="contact_form_holder">
+    <div class="row">
+        <div class="col-md-3"></div>
+        <div class="col-md-9"><span class="required"> * Required fields</span></div>
+    </div>
+    <div class="row">
+        <div class="col-md-3"><div class="form_label">Name</div></div>
+        <div class="col-md-9"><div class="field">[text* your-name]</div></div>
+    </div>
+    <div class="row">
+        <div class="col-md-3"><div class="form_label">Title</div></div>
+        <div class="col-md-9"><div class="field">[text your-title]</div></div>
+    </div>
+    <div class="row">
+        <div class="col-md-3"><div class="form_label">Company</div></div>
+        <div class="col-md-9"><div class="field">[text your-company]</div></div>
+    </div>
+    <div class="row">
+        <div class="col-md-3"><div class="form_label">Email</div></div>
+        <div class="col-md-9"><div class="field">[email* your-email]</div></div>
+    </div>
+    <div class="row">
+        <div class="col-md-3"><div class="form_label">Message</div></div>
+        <div class="col-md-9"><div class="field">[textarea your-message]</div></div>
+    </div>
+    <div class="submit_btn">[submit "Send"]</div>
+</div>
---
+++ Forms/Contact Forms 7 - examples/contact-form-simple-cf7.php
@@ -0,0 +1,37 @@
+<!-- Contact form with labels -->
+<div class="contact_form">
+    <div class="wrap_field">
+        <label for="full-name">FULL NAME</label>
+        <div class="field">[text* full-name id:full-name placeholder "FULL NAME"]</div>
+    </div>
+    <div class="wrap_field">
+        <label for="phone">TEL</label>
+        <div class="field">[tel* phone id:phone placeholder "TEL"]</div>
+    </div>
+    <div class="wrap_field">
+        <label for="your-email">EMAIL</label>
+        <div class="field">[email* your-email id:your-email placeholder "EMAIL"]</div>
+    </div>
+    <div class="wrap_field">
+        <label for="subject">SUBJECT</label>
+        <div class="field">[text mail-subject id:subject placeholder "SUBJECT"]</div>
+    </div>
+    <div class="wrap_field">
+        <label for="message">CONTENT</label>
+        <div class="field textarea">[textarea message id:message placeholder "CONTENT"]</div>
+    </div>
+    <div class="wrap_field">
+        <div class="field submit">[submit "SEND"]</div>
+    </div>
+</div>
+
+
+<!-- Simple Contact form -->
+<div class="side_contact_form">
+<h2 class="widget_title">Contact Us</h2>
+<div class="field">[text* fullname placeholder "Full Name*"]</div>
+<div class="field">[text* phone placeholder "phone*"]</div>
+<div class="field">[email* email placeholder "Email*"]</div>
+<div class="field">[textarea message placeholder "Message"]</div>
+<div class="field submit">[submit "Send"]</div>
+</div>
